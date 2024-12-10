40ba39e90ee692495c2a168cd4140e7fc54d10f9
HermanJasser <herjas00989@stud.noroff.no>
2024-12-10 18:01:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
 });*/
 
 
-import { defineConfig } from 'vite';
+/*import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
@@ -57,4 +57,41 @@ export default defineConfig({
     },
   },
   publicDir: 'public', // Sørger for at `_redirects` blir inkludert
+});*/
+
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: '/', // Viktig for Netlify
+  publicDir: 'public',
+  build: {
+    target: 'esnext', // Støtter moderne JS-funksjoner
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, '/login/index.html'),
+        register: resolve(__dirname, '/register/index.html'),
+        myPage: resolve(__dirname, '/minside/index.html'),
+        newPost: resolve(__dirname, '/minside/nyannonse/index.html'),
+        editProfile: resolve(__dirname, '/minside/redigerprofil/index.html'),
+        editListing: resolve(__dirname, '/minside/redigerannonse/index.html'),
+        myWins: resolve(__dirname, '/minside/minekjop/index.html'),
+        myBids: resolve(__dirname, '/minside/minebud/index.html'),
+        singleListing: resolve(__dirname, '/enkelpost/index.html'),
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
+

cda2bd4c11fa10ce1bec742244b5a9cdda6e0953
HermanJasser <herjas00989@stud.noroff.no>
2024-12-10 17:37:06 +0100

    update i vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+/*import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.', // Angir roten til prosjektet ditt (kan justeres hvis nødvendig)
@@ -17,6 +17,32 @@ export default defineConfig({
     },
   },
   publicDir: 'public', // Mappen for offentlige filer som ikke må bygges
+});*/
+
+
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: '.', // Rotmappen til prosjektet
+  build: {
+    target: 'esnext',
+    outDir: 'dist', // Output-mappen for bygging
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  publicDir: 'public', // Sørger for at `_redirects` blir inkludert
 });
-
-
